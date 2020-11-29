620f2e1c44abfd49e4f4b2bf419fcb66444ae69a
jmanalo26 <63303459+jmanalo26@users.noreply.github.com>
2020-11-28 22:10:37 -0600

    Level 1 Battle with Graphics added

--- src/main/games/shooter/level1/Level1.java
+++ src/main/games/shooter/level1/Level1.java
@@ -4,15 +4,21 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import main.games.shooter.Player;
 import main.gui.gameovermenu.GameOverMenu;
 import main.gui.gamewonmenu.GameWonMenu;
 
+import javafx.scene.image.*;
+import java.awt.*;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -23,7 +29,9 @@ public class Level1 extends Application {
 
     private Pane root = new Pane();
 
-    private Entity playerEntity = new Entity(275, 450, 15, 15, "player", Color.BLUE);
+    private Entity playerEntity = new Entity(275, 450, 25, 25, "player", Color.CYAN);
+
+    private Entity HPBar;
 
     private Player player;
 
@@ -46,13 +54,19 @@ public class Level1 extends Application {
      * @return Shooting game
      */
     private Parent createRoot() {
+        playerEntity.setId("player");
+        Image playerImage = new Image(getClass().getResource("images/player.png").toExternalForm());
+        ImagePattern i = new ImagePattern(playerImage);
+        playerEntity.setFill(i);
         root.setPrefSize(600, 575);
         root.getChildren().add(playerEntity);
-        player = new Player(playerEntity.getX(), playerEntity.getY(), 60, 6);
+        player = new Player(playerEntity.getX(), playerEntity.getY(), 5, 6);
         //Add animation timer to animate bullet
         timer.start();
         addEnemies();
         addObstacles();
+        addHealthBar();
+        root.setId("pane");
         return root;
     }
 
@@ -66,18 +80,43 @@ public class Level1 extends Application {
     }
 
     private void addObstacles(){
-         Entity obstacle = new Entity(200, 200, 20, 20, "obstacle", Color.GREEN);
-         root.getChildren().add(obstacle);
+        Random random = new Random();
+        Image obsImage = new Image(getClass().getResource("images/tree.png").toExternalForm());
+        ImagePattern obs = new ImagePattern(obsImage);
+        for (int i = 0; i < 20; i++) {
+            int num = random.nextInt(400) + 50;
+            int num2 = 400 - random.nextInt(300);
+            Entity obstacle = new Entity(num, num2, 30, 30, "obstacle", Color.GREEN);
+            obstacle.setId("tree");
+            obstacle.setFill(obs);
+            root.getChildren().add(obstacle);
+        }
+    }
+
+    private void addHealthBar(){
+        HPBar = new Entity(10, 530, 200, 15, "HP", Color.CYAN);
+        Entity HPBorder = new Entity(0, 500, 1000, 100, "outline", Color.BLACK);
+        HPBorder.setOpacity(0.3);
+        root.getChildren().addAll(HPBar, HPBorder);
+        //root.getChildren().add(HPBar);
     }
 
     /**
      * Add enemies, currently set to 10
      */
     private void addEnemies() {
+        Image enemyImage = new Image(getClass().getResource("images/zombie.png").toExternalForm());
+        ImagePattern en = new ImagePattern(enemyImage);
         for (int i = 0; i < 10; i++) {
-            Entity enemy = new Entity(30 + i * 50, 50, 20, 20, "enemy", Color.RED);
-            Entity leftEnemy = new Entity(30, 90 + i * 30, 20, 20, "enemyLeft", Color.RED);
-            Entity rightEnemy = new Entity(30 + 9 * 50, 90 + i * 30, 20, 20, "enemyRight", Color.RED);
+            Entity enemy = new Entity(30 + i * 50, 50, 25, 25, "enemy", Color.RED);
+            Entity leftEnemy = new Entity(30, 90 + i * 30, 25, 25, "enemyLeft", Color.RED);
+            Entity rightEnemy = new Entity(30 + 9 * 50, 90 + i * 30, 25, 25, "enemyRight", Color.RED);
+            enemy.setId("enemy");
+            leftEnemy.setId("enemy");
+            rightEnemy.setId("enemy");
+            enemy.setFill(en);
+            leftEnemy.setFill(en);
+            rightEnemy.setFill(en);
             root.getChildren().add(enemy);
             root.getChildren().add(leftEnemy);
             root.getChildren().add(rightEnemy);
@@ -93,6 +132,7 @@ public class Level1 extends Application {
     public void start(Stage primaryStage) {
         enemyDead.set(0);
         Scene scene = new Scene(createRoot());
+        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         System.out.println("Player Starting HP: " + player.getHP());
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()) {
@@ -167,6 +207,8 @@ public class Level1 extends Application {
         void moveRight() {
             setTranslateX(getTranslateX() + 7);
         }
+
+
     }
 
     /**
@@ -175,41 +217,89 @@ public class Level1 extends Application {
      * @param entity Bullet
      */
     private void shoot(Entity entity) {
-        Entity s = new Entity((int) entity.getTranslateX() + 10, (int) entity.getTranslateY(), 3, 20, entity.type + "bullet", Color.BLACK);
+        Entity s = new Entity((int) entity.getTranslateX() + 20, (int) entity.getTranslateY(), 2, 20, entity.type + "bullet", Color.WHITE);
         root.getChildren().add(s);
+    }
+
+    /**
+     * Method to increment Player HP, player is dead if 0
+     */
+    private void incrementHP() {
+        player.setHP(player.getHP() - 1);
+        System.out.println(player.getHP() + " HP remaining");
+        HPBar.setWidth(HPBar.getWidth() - 40);
+        if (player.getHP() <= 0) {
+            playerEntity.dead = true;
+            stage.close();
+        }
     }
 
     /**
      * Update method, has functionality for bullet hits, enemy movement, entity deaths
      */
     private void update() {
-        time += 0.015;
-        //System.out.println(time);
+        time += 1;
+        if (time % 1000 == 0){
+            addEnemies();
+        }
         entities().forEach(s -> {
             switch (s.type) {
                 //Simulate Enemy movement and shooting
                 case "enemy":
-                    if (time > 2) {
+                    if (time > 200) {
                         if (Math.random() < 0.015) {
                             s.moveDown();
+                            entities().stream().filter(e -> e.type.equals("player")).forEach(p -> {
+                                if (s.getBoundsInParent().intersects(p.getBoundsInParent())) {
+                                    incrementHP();
+                                    s.dead = true;
+                                }
+                            });
+                            entities().stream().filter(e -> e.type.equals("outline")).forEach(e -> {
+                                if (s.getBoundsInParent().intersects(e.getBoundsInParent())) {
+                                    s.dead = true;
+                                }
+                            });
                         }
                     }
                     break;
 
                 case "enemyLeft":
-                    if (time > 2) {
+                    if (time > 200) {
                         if (Math.random() < 0.015) {
                             s.moveRight();
                             s.moveDown();
+                            entities().stream().filter(e -> e.type.equals("player")).forEach(p -> {
+                                if (s.getBoundsInParent().intersects(p.getBoundsInParent())) {
+                                    incrementHP();
+                                    s.dead = true;
+                                }
+                            });
+                            entities().stream().filter(e -> e.type.equals("outline")).forEach(e -> {
+                                if (s.getBoundsInParent().intersects(e.getBoundsInParent())) {
+                                    s.dead = true;
+                                }
+                            });
                         }
                     }
                     break;
 
                 case "enemyRight":
-                    if (time > 2) {
+                    if (time > 200) {
                         if (Math.random() < 0.015) {
                             s.moveLeft();
                             s.moveDown();
+                            entities().stream().filter(e -> e.type.equals("player")).forEach(p -> {
+                                if (s.getBoundsInParent().intersects(p.getBoundsInParent())) {
+                                    incrementHP();
+                                    s.dead = true;
+                                }
+                            });
+                            entities().stream().filter(e -> e.type.equals("outline")).forEach(e -> {
+                                if (s.getBoundsInParent().intersects(e.getBoundsInParent())) {
+                                    s.dead = true;
+                                }
+                            });
                         }
                     }
                     break;
@@ -288,7 +378,7 @@ public class Level1 extends Application {
             return s.dead;
         });
 
-        if (enemyDead.get() == 10) {
+        if (enemyDead.get() == 25) {
             System.out.println("All enemies dead");
             stage.close();
 
