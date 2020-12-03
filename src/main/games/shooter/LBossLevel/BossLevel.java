8910cfe9ae333227612ab46b65a9dde50c714518
Liam <lpcbusiness777@gmail.com>
2020-12-02 18:15:50 -0600

    Reload mechanic and some images added

--- src/main/games/shooter/LBossLevel/BossLevel.java
+++ src/main/games/shooter/LBossLevel/BossLevel.java
@@ -2,21 +2,18 @@ package main.games.shooter.LBossLevel;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import main.MainApplication;
+import main.games.shooter.Player;
 import main.gui.gameovermenu.GameOverMenu;
 import main.gui.gamewonmenu.GameWonMenu;
-import main.gui.startmenu.StartMenu;
 
-import java.util.Random;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -29,6 +26,12 @@ public class BossLevel extends Application {
     private Pane root = new Pane();
 
     private BossLevel.Entity playerEntity = new BossLevel.Entity(275, 450, 15, 15, "player", Color.BLUE);
+
+    private Entity HPBar;
+
+    private Player player;
+
+    private static Entity[] bullets = new Entity[6];
 
     private double time = 0;
 
@@ -54,10 +57,18 @@ public class BossLevel extends Application {
      * @return Shooting game
      */
     private Parent createRoot() {
+        playerEntity.setId("player");
+        Image playerImage = new Image(getClass().getResource("images/player.png").toExternalForm());
+        ImagePattern i = new ImagePattern(playerImage);
+        playerEntity.setFill(i);
         root.setPrefSize(600, 575);
         root.getChildren().add(playerEntity);
+        player = new Player(playerEntity.getX(), playerEntity.getY(), 5, 6);
         //Add animation timer to animate bullet
         timer.start();
+        addHealthBar();
+        displayAmmo();
+        root.setId("pane");
         addBoss();
         return root;
     }
@@ -67,17 +78,105 @@ public class BossLevel extends Application {
      *
      * @return List of entities
      */
-    private List<BossLevel.Entity> entities() {
-        return root.getChildren().stream().map(n -> (BossLevel.Entity) n).collect(Collectors.toList());
+    private List<Entity> entities() {
+        return root.getChildren().stream().map(n -> (Entity) n).collect(Collectors.toList());
     }
 
     /**
      * Adds the single boss enemy
      */
     private void addBoss() {
-        BossLevel.Entity enemy = new BossLevel.Entity(280, 50, 60, 30, "enemy", Color.RED);
+        Entity enemy = new Entity(280, 50, 60, 30, "enemy", Color.RED);
         root.getChildren().add(enemy);
     }
+
+    /**
+     * Creates a health bar entity at the bottom of the game window
+     */
+    private void addHealthBar() {
+        HPBar = new Entity(10, 530, 200, 15, "HP", Color.RED);
+        Entity HPBorder = new Entity(0, 500, 1000, 100, "outline", Color.BLACK);
+        HPBorder.setOpacity(0.3);
+        root.getChildren().addAll(HPBar, HPBorder);
+        //root.getChildren().add(HPBar);
+    }
+
+    /**
+     * Displays ammo count
+     */
+    private void displayAmmo() {
+        for (int i = 0; i < player.getAmmo(); i++) {
+            Entity ammo = new Entity(400 + (20 * i), 530, 3, 20, "ammo" + i, Color.BLACK);
+            bullets[i] = ammo;
+            root.getChildren().add(ammo);
+        }
+    }
+
+    /**
+     * Depletes ammo when the player fires
+     */
+    private void depleteAmmo() {
+        if (player.getAmmo() < 6) {
+            int ammoUsed = 5 - player.getAmmo();
+            bullets[ammoUsed].dead = true;
+        }
+    }
+
+    /**
+     * Ammo reloads for player
+     */
+    private void addAmmo() {
+        if (player.getAmmo() < 6) {
+            int ammoUsed = 6 - player.getAmmo();
+            for (int i = 0; i < ammoUsed; i++) {
+                Entity ammo = new Entity(400 + (20 * i), 530, 3, 20, "ammo" + i, Color.BLACK);
+                bullets[i] = ammo;
+                root.getChildren().add(ammo);
+            }
+        }
+    }
+
+    /**
+     * Method to de-increment Player HP, player is dead if 0
+     */
+    private void deIncrementHP() {
+        player.setHP(player.getHP() - 1);
+        System.out.println("Player Hit; " + player.getHP() + " HP remaining");
+        HPBar.setWidth(HPBar.getWidth() - 40);
+        if (player.getHP() <= 0) {
+            playerEntity.dead = true;
+            System.out.println("Player has died");
+            stage.close();
+            timer.stop();
+            GameOverMenu gameOverMenu = new GameOverMenu();
+            Stage gameOverMenuStage = new Stage();
+            try {
+                gameOverMenu.start(gameOverMenuStage);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Method to restore player HP when picking up health item
+     */
+    private void incrementHP() {
+        player.setHP(player.getHP() + 1);
+        HPBar.setWidth(HPBar.getWidth() + 40);
+    }
+
+    /**
+     * Create bullet entity objects
+     *
+     * @param entity Bullet
+     */
+    private void createBullets(Entity entity) {
+        Entity s = new Entity((int) entity.getTranslateX() + 20, (int) entity.getTranslateY(), 2, 20, entity.type + "bullet", Color.BLACK);
+        root.getChildren().add(s);
+    }
+
 
     /**
      * Start method for shooting game
@@ -86,20 +185,33 @@ public class BossLevel extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-
         enemyDead.set(0);
         Scene scene = new Scene(createRoot());
+        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()) {
                 case A:
                     playerEntity.moveLeft();
+                    player.setX(playerEntity.getTranslateX());
                     break;
+
                 case D:
                     playerEntity.moveRight();
+                    player.setX(playerEntity.getTranslateX());
                     break;
+
                 case SPACE:
-                    playerShoot(playerEntity);
+                    if (player.getAmmo() > 0) {
+                        createBullets(playerEntity);
+                        player.setAmmo(player.getAmmo() - 1);
+                        System.out.println("Current Ammo: " + player.getAmmo());
+                        depleteAmmo();
+                    }
                     break;
+                case R:
+                    addAmmo();
+                    player.setAmmo(6);
+                    System.out.println("Player Reloaded; Current Ammo: " + player.getAmmo());
             }
         });
 
@@ -329,22 +441,8 @@ public class BossLevel extends Application {
                 case "enemytribulletL":
                     s.moveDiagonalLeftDown();
                     if (s.getBoundsInParent().intersects(playerEntity.getBoundsInParent())) {
-                        playerEntity.dead = true;
                         s.dead = true;
-                        stage.close();
-
-
-                        timer.stop();
-
-                        //launches game over menu when game is lost
-                        GameOverMenu gameOverMenu = new GameOverMenu();
-                        Stage gameOverMenuStage = new Stage();
-                        try {
-                            gameOverMenu.start(gameOverMenuStage);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        deIncrementHP();
                     }
                     break;
 
@@ -352,21 +450,8 @@ public class BossLevel extends Application {
                 case "enemytribulletR":
                     s.movieDiagonalRightDown();
                     if (s.getBoundsInParent().intersects(playerEntity.getBoundsInParent())) {
-                        playerEntity.dead = true;
                         s.dead = true;
-                        stage.close();
-
-                        timer.stop();
-
-                        //launches game over menu when game is lost
-                        GameOverMenu gameOverMenu = new GameOverMenu();
-                        Stage gameOverMenuStage = new Stage();
-                        try {
-                            gameOverMenu.start(gameOverMenuStage);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        deIncrementHP();
                     }
                     break;
 
@@ -374,21 +459,8 @@ public class BossLevel extends Application {
                 case "enemybullet":
                     s.moveDown();
                     if (s.getBoundsInParent().intersects(playerEntity.getBoundsInParent())) {
-                        playerEntity.dead = true;
                         s.dead = true;
-                        stage.close();
-
-                        timer.stop();
-
-                        //launches game over menu when game is lost
-                        GameOverMenu gameOverMenu = new GameOverMenu();
-                        Stage gameOverMenuStage = new Stage();
-                        try {
-                            gameOverMenu.start(gameOverMenuStage);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        deIncrementHP();
                     }
                     break;
 
