0e7cc68f0851bcba5d90b0c63f0702d1e0511f14
Anunay Jujjavarapu <ajujjavarapu@gmail.com>
2020-12-07 16:07:06 -0800

    Formatted the TileManager so it can work with any tilesheet and any level. Also, added some functionality to retrieve the information needed for the backend.

--- src/main/gui/mazefront/TileManager.java
+++ src/main/gui/mazefront/TileManager.java
@@ -2,6 +2,7 @@ package main.gui.mazefront;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import main.gui.mazefront.util.Block;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -24,7 +25,7 @@ public class TileManager {
 
     private Pane root;
     private String path;
-    private SpriteSheet spriteSheet;
+    private SpriteSheet tileSheet;
 
 
 //    public TileManager(Pane root) {
@@ -33,70 +34,28 @@ public class TileManager {
 
     public TileManager(Pane root, SpriteSheet spriteSheet, String xmlPathToTileSheet) {
         this.root = root;
-        this.spriteSheet = spriteSheet;
+        this.tileSheet = spriteSheet;
         this.path = xmlPathToTileSheet;
     }
 
-    public void buildMap() {
-//        try {
-//            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newDefaultInstance();
-//            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-//            Document document = builder.parse(new File(getClass().getClassLoader().getResource(path).toURI()));
-//
-//            document.getDocumentElement().normalize();
-//
-//            NodeList nl = document.getElementsByTagName("tileset");
-//            Node node = nl.item(0);
-//            Element element = (Element) node;
-//
-//            blockWidth = Integer.parseInt(element.getAttribute("tilewidth"));
-//            blockHeight = Integer.parseInt(element.getAttribute("tileheight"));
-//            totalCount = Integer.parseInt(element.getAttribute("tilecount"));
-//            columns = Integer.parseInt(element.getAttribute("columns"));
-//
-//            nl = document.getElementsByTagName("layer");
-//            layers = nl.getLength();
-//            data = new String[layers];
-//
-//            for (int layer = 0; layer < layers; ++layer) {
-//                node = nl.item(layer);
-//                element = (Element) node;
-//                if (layer == 0) {
-//                    width = Integer.parseInt(element.getAttribute("width"));
-//                    height = Integer.parseInt(element.getAttribute("height"));
-//                }
-//
-//                data[layer] = element.getElementsByTagName("data").item(0).getTextContent();
-//                System.out.println("-----------------------------------------------------------------\n" + data[layer]);
-//
-//            }
-//
-//
-//        } catch (Exception e) {
-//            System.out.println("Exception Thrown!");
-//        }
+    public TileManager(SpriteSheet tileSheet, String xmlPathToTileSheet){
+        this.root = null;
+        this.tileSheet = tileSheet;
+        this.path = xmlPathToTileSheet;
+    }
 
-        init();
+//    public void getMapData(){
 //
-//        Pane level = new Pane();
-//        ImageView temp = getImageByTileId(49);
-//        temp.setX(0);
-//        temp.setY(0);
-//        ImageView temp2 = getImageByTileId(50);
-//        temp2.setX(16);
-//        temp2.setY(0);
-//        level.getChildren().addAll(temp, temp2);
-//        root.getChildren().add(level);
+//    }
+
+    public void buildMap() {
+        init();
 
         for (String layer : data) {
             Pane pane = new Pane();
             setPane(pane, layer);
             root.getChildren().add(pane);
-//            System.out.println(layer);
         }
-//        Pane pane = new Pane();
-//        setPane(pane, data[0]);
-//        root.getChildren().add(pane);
     }
 
     private void init() {
@@ -129,8 +88,6 @@ public class TileManager {
                 }
 
                 data[layer] = element.getElementsByTagName("data").item(0).getTextContent();
-//                System.out.println("-----------------------------------------------------------------\n" + data[layer]);
-
             }
 
 
@@ -140,13 +97,13 @@ public class TileManager {
     }
 
     public ImageView getImageByTileId(int id) {
-        int row = id % 23;
-        int column = (int) Math.ceil(id / 23.0);
+        int row = id % columns;
+        int column = (int) Math.ceil(id / (double) columns);
 //        int offsetX = blockWidth * (row - 1);
 //        int offsetY = blockHeight * (column - 1);
 //        Stream.of(row, column, offsetX, offsetY).forEach(System.out::println);
 //        ImageView ss = spriteSheet.getImageAt(row, column);
-        return spriteSheet.getImageAt(row, column);
+        return tileSheet.getImageAt(row, column);
     }
 
     private void setPane(Pane pane, String layerData) {
@@ -161,11 +118,8 @@ public class TileManager {
                 int row = Math.floorDiv(counter, height);
                 int offsetX = col * blockWidth;
                 int offsetY = row * blockHeight;
-
-                System.out.println("column: " + col);
-                System.out.println("row: " + row);
-                System.out.println("offsetX: " + offsetX);
-                System.out.println("offsetY: " + offsetY);
+//                System.out.println("Row: " + row);
+//                System.out.println("Col: " + col);
 
                 tempImg.setX(offsetX);
                 tempImg.setY(offsetY);
@@ -179,5 +133,42 @@ public class TileManager {
         }
     }
 
+    public Block[][] getBlocks() {
+        init();
+        Block[][] blockArray = new Block[width][height];
+        for (int i = 0; i < data[0].length(); i++) {
+            String[] individualData = data[0].split(",");
+            int counter = 0;
+
+            for (String tile : individualData) {
+                try {
+                    Integer blockId = Integer.parseInt(tile.trim());
+                    ImageView tempImg = getImageByTileId(blockId);
+                    int col = counter % width;
+                    int row = Math.floorDiv(counter, height);
+                    int offsetX = col * blockWidth;
+                    int offsetY = row * blockHeight;
+
+                    tempImg.setX(offsetX);
+                    tempImg.setY(offsetY);
+
+                    Block block;
+                    if (blockId == 0 || blockId == 1) {
+                        block = new Block(tempImg, row, col, 1);
+                    } else {
+                        block = new Block(tempImg, row, col, 0);
+                    }
+                    blockArray[row][col] = block;
+                    counter++;
+
+                } catch (RuntimeException e) {
+                    System.out.println("Exception thrown: " + tile);
+                    continue;
+                }
+            }
+        }
+
+        return blockArray;
+    }
 
 }
---
+++ src/main/gui/mazefront/util/Block.java
@@ -0,0 +1,34 @@
+package main.gui.mazefront.util;
+
+import javafx.scene.image.ImageView;
+
+public class Block {
+    private final ImageView img;
+    private final int row;
+    private final int column;
+    private final int value;
+
+
+    public Block(ImageView img, int row, int column, int value) {
+        this.img = img;
+        this.row = row;
+        this.column = column;
+        this.value = value;
+    }
+
+    public ImageView getImg() {
+        return img;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
